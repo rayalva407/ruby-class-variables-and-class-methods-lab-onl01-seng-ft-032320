cb9480d0062f7a7b9e7f3b25404135178370c7ed
Ray Alva <rayalva407@gmail.com>
2020-04-02 21:00:46 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,31 @@
+class Song
+  
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  
+  def initialize(name, artist, genre)
+    @name, @artist, @genre = name, artist, genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.genres
+    @@genres.uniq
+  end
+  
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def self.genre_count
+    
+  end
+  
+end
